703f045c3c429abe091c7bd1c1d0013860061022
pranshu kumar <pranshukumar9519@gmail.com>
2025-08-07 00:44:18 +0530

    depluy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,6 @@ import App from './App.jsx';
 import './index.css';
 import { CartProvider } from './context/CartContext.jsx';
 import { SocketProvider } from './context/SocketContext.jsx';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-
-// --- डीबगिंग अलर्ट को अभी के लिए हटा दें ---
-// const apiUrl = import.meta.env.VITE_API_URL;
-// alert("The API URL is: " + apiUrl);
-// ------------------------------------
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
