daa442c79305d42510cc0eb29add59042f0307a0
pranshu kumar <pranshukumar9519@gmail.com>
2025-08-07 00:29:09 +0530

    Add debug alert for API URL

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
-import { SocketProvider } from './context/SocketContext.jsx';
-import { CartProvider } from './context/CartContext.jsx';
 import './index.css';
-import axios from 'axios';
+import { CartProvider } from './context/CartContext.jsx';
+import { SocketProvider } from './context/SocketContext.jsx';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+// --- डीबगिंग के लिए यह अलर्ट जोड़ें ---
+const apiUrl = import.meta.env.VITE_API_URL;
+alert("API URL being used is: " + apiUrl);
+// ------------------------------------
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> {/* ✅ Yahin hona chahiye */}
-      <SocketProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </SocketProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <SocketProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </SocketProvider>
+  </React.StrictMode>,
 );
-// client/src/main.jsx
-
-
-axios.defaults.baseURL = import.meta.env.VITE_API_URL;
