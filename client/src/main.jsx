d40087e014661d7f26116afed1d72bec4cfd61ba
pranshu kumar <pranshukumar9519@gmail.com>
2025-08-07 00:45:51 +0530

    u

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
-import { CartProvider } from './context/CartContext.jsx';
+import { BrowserRouter } from 'react-router-dom';
 import { SocketProvider } from './context/SocketContext.jsx';
+import { CartProvider } from './context/CartContext.jsx';
+import './index.css';
+import axios from 'axios';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SocketProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </SocketProvider>
-  </React.StrictMode>,
+    <BrowserRouter> {/* ✅ Yahin hona chahiye */}
+      <SocketProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </SocketProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
+// client/src/main.jsx
+
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
